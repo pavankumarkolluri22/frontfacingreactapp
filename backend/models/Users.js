dd9b60c59cc611a74b033c5c79e47cd6662b0532
Venkatram Pavan Kumar <venkatram.kolluri@changehealthcare.com>
2020-04-10 12:18:22 -0400

    initial project setup

---
+++ backend/models/Users.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phoneNumber: {
+    type: Number,
+    required: false
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
