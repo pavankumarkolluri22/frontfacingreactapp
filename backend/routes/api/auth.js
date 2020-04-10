29d9db320f9fa2ad1c184ae347e536dcc525ab6b
Venkatram Pavan Kumar <venkatram.kolluri@changehealthcare.com>
2020-04-10 12:21:53 -0400

    commit1

--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 // @route auth route 
 // access Public
-router.get('/', (req,res) => res.send('Auth route'));
+router.get('/', (req, res) => res.send('Auth route'));
+
+
 
 module.exports = router;
